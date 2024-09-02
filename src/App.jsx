7f30fee1f7d0a4dbcd7ac411b4b8c040a5965e94
joshua <j.j.jatto@gmail.com>
2024-09-02 02:05:27 +0100

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import UpdateProfile from "./components/authentication/UpdateProfile";
 import PrivateRoute from "./components/authentication/privateRoute";
 
-import "../src/styles.css";
 import ForgotPassword from "./components/authentication/ForgotPassword";
 function App() {
   return (
@@ -24,7 +23,7 @@ function App() {
           
             <Route path="/forgot-password" element={<ForgotPassword />} />
             <Route path="/signup" element={<SignUpForm />} />
-            <Route exact path="/" element={<LoginForm />} />
+            <Route  path="/" element={<LoginForm />} />
             <Route path="/update-profile" element={<UpdateProfile />} />
           </Routes>
         </AuthProvider>
